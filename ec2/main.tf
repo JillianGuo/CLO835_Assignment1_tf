fc45181f17bbb19501a5581c9525a46983c2d3eb
J.Guo <jiyun.guo@gmail.com>
2025-02-02 14:29:35 -0500

    Modify key pair name in main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -44,23 +44,12 @@ module "globalvars" {
   source = "../modules/globalvars"
 }
 
-# IAM Role for EC2 (Assume Role Policy)
-data "aws_iam_policy_document" "ec2_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
-
 # Reference subnet provisioned by 01-Networking 
 resource "aws_instance" "my_amazon" {
   ami                         = data.aws_ami.latest_amazon_linux.id
   instance_type               = lookup(var.instance_type, var.env)
   key_name                    = aws_key_pair.my_key.key_name
-  vpc_security_group_ids             = [aws_security_group.my_sg.id]
+  vpc_security_group_ids      = [aws_security_group.my_sg.id]
   associate_public_ip_address = true
   
   lifecycle {
@@ -77,7 +66,7 @@ resource "aws_instance" "my_amazon" {
 
 # Adding SSH key to Amazon EC2
 resource "aws_key_pair" "my_key" {
-  key_name   = local.name_prefix
+  key_name   = "assignment1"
   public_key = file("${local.name_prefix}.pub")
 }
 
